00362f1272cae596347db7da992d1e3a018f5bbe
Luis <isan.qbik@gmail.com>
2024-10-16 23:58:36 +0200

    add method for program continues running

--- TestAppRepository/TestAppRepository/Program.cs
+++ TestAppRepository/TestAppRepository/Program.cs
@@ -9,70 +9,77 @@
             int selectedOption = 0;
             string[] contacts = ["James Dohar", "Mathew Willy"];
 
-            Console.WriteLine("Select an option, enter a option number:");
-            Console.WriteLine("1 - Add new contact:");
-            Console.WriteLine("2 - View all contacts:");
-            Console.WriteLine("3 - Search contact:");
-            Console.WriteLine("4 - Delete contact:");
-            Console.WriteLine("5 - Exit program:");
+            bool programOn = true;
 
-            string answer = Console.ReadLine();
-            selectedOption = Convert.ToInt32(answer);
+            while (programOn)
+            {
+                Console.WriteLine("Select an option, enter a option number:");
+                Console.WriteLine("1 - Add new contact:");
+                Console.WriteLine("2 - View all contacts:");
+                Console.WriteLine("3 - Search contact:");
+                Console.WriteLine("4 - Delete contact:");
+                Console.WriteLine("5 - Exit program:");
 
-            Console.WriteLine($"Option selected:{selectedOption}");
+                string answer = Console.ReadLine();
+                selectedOption = Convert.ToInt32(answer);
 
-            switch (selectedOption) { 
-                case 1:
-                    Console.WriteLine("Add new contact name:");
-                    Contact contact = new Contact();
-                    contact.Name = Console.ReadLine();
-                    Console.WriteLine($"new contact added {contact.Name}");
-                    contacts = contacts.Append(contact.Name).ToArray();
-                    Console.WriteLine("added contact");
-                    break;
-                case 2:
-                    Console.WriteLine("All contact list:");
-                    if (contacts.Length > 0)
-                    {
-                        foreach (string c in contacts)
+                Console.WriteLine($"Option selected:{selectedOption}");
+
+                switch (selectedOption)
+                {
+                    case 1:
+                        Console.WriteLine("Add new contact name:");
+                        Contact contact = new Contact();
+                        contact.Name = Console.ReadLine();
+                        Console.WriteLine($"new contact added {contact.Name}");
+                        contacts = contacts.Append(contact.Name).ToArray();
+                        Console.WriteLine("added contact");
+                        break;
+                    case 2:
+                        Console.WriteLine("All contact list:");
+                        if (contacts.Length > 0)
                         {
-                            Console.WriteLine(c);
+                            foreach (string c in contacts)
+                            {
+                                Console.WriteLine(c);
+                            }
                         }
-                    }
-                    else {
-                        Console.WriteLine("Contact list is empty");
-                    }
-                    break;
-                case 3:
-                    Console.WriteLine("Search contact name:");
-                    Contact searchingContact = new Contact();
-                    searchingContact.Name = Console.ReadLine();
-                    Console.WriteLine($"searching {searchingContact.Name}...");
-                    
-                    string stringToFind = searchingContact.Name;
-                    string result = Array.Find(contacts, element => element == stringToFind);
+                        else
+                        {
+                            Console.WriteLine("Contact list is empty");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Search contact name:");
+                        Contact searchingContact = new Contact();
+                        searchingContact.Name = Console.ReadLine();
+                        Console.WriteLine($"searching {searchingContact.Name}...");
 
-                    if (result == stringToFind)
-                    {
-                        Console.WriteLine($"{result} is in the contact list");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{result} not found in the contact list :(");
-                    }
+                        string stringToFind = searchingContact.Name;
+                        string result = Array.Find(contacts, element => element == stringToFind);
 
-                    break;
-                case 4:
-                    Console.WriteLine("Write contact name to delete:");
-                    Contact contactToDelete = new Contact();
-                    contactToDelete.Name = Console.ReadLine();
-                    Console.WriteLine($"{contactToDelete.Name} has been removed from contact list");
-                    break;
-                case 5:
-                    Console.WriteLine("Good bye");
-                    break;
+                        if (result == stringToFind)
+                        {
+                            Console.WriteLine($"{result} is in the contact list");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{result} not found in the contact list :(");
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Write contact name to delete:");
+                        Contact contactToDelete = new Contact();
+                        contactToDelete.Name = Console.ReadLine();
+                        Console.WriteLine($"{contactToDelete.Name} has been removed from contact list");
+                        break;
+                    case 5:
+                        Console.WriteLine("Good bye");
+                        programOn = false;
+                        break;
+                }
             }
-
         }
     }
 }
