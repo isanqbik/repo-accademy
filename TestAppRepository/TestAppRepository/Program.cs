466744ae20835cbcff3aa55d335b8e059ddf09db
Luis <isan.qbik@gmail.com>
2024-10-16 12:35:12 +0200

    Prepare project

---
+++ TestAppRepository/TestAppRepository/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAppRepository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string contacts;
+
+            contacts = "Pepe";
+
+            Console.WriteLine($"Add Contact!{contacts}");
+        }
+    }
+}
